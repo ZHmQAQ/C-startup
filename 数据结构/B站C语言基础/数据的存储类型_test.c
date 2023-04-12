40b0465aa7c6531351e7f8383430dcddf1c01261
ZHm <zhm019911@qq.com>
2023-04-12 09:34:39 +0800

    学完了浮点在内存中的存储
    更改了文件名

---
+++ 数据结构/B站C语言基础/数据的存储类型_test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    // //##########整型练习##########
+
+    // //练习 1
+	// char a = -1;
+	// signed char b = -1;
+	// unsigned char c = -1;
+	// printf("%d\n%d\n%d\n", a,b,c);
+
+    // //练习 2
+    // char a = -128;
+    // printf("%u\n", a);
+
+    // //练习 3
+    // char a = 128;
+    // printf("%u\n", a);
+
+    // //练习 4
+    // int i = -20;
+    // unsigned int j = 10;
+    // printf("%d\n",i+j);
+
+    // //练习 5
+    // unsigned int i;
+    // for(i = 9;i>=0;i--)
+    // {
+    //     printf("%u\n",i);
+    // }
+
+    // //练习 6
+    // char a[1000];
+    // int i;
+    // for(i = 0; i < 1000; i++)
+    //     a[i] = -1 - i;
+    // printf("%d",strlen(a));
+
+    // //练习 7
+    // unsigned char i = 0;
+    // for (i = 0; i <= 255; i++)
+    //     printf("helloworld\n");
+    // return 0;
+
+    // //##########浮点练习##########
+
+    // //练习1
+    // int n = 9;
+    // float *pFloat = (float *)&n;//这里最好用强制类型转换
+    // printf("      n的值：%d\n", n);
+    // printf("*pFloat的值：%f\n", *pFloat);
+    // *pFloat = 9.0;
+    // printf("      n的值：%d\n", n);
+    // printf("*pFloat的值：%f\n", *pFloat);
+
+
+    
+}
+
